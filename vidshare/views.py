05e6083cc6ce1acfbb3bde9f8cf03da3f2871cc1
Kacper Słomski <kacperslomski7@gmail.com>
2022-11-13 18:30:03 +0100

    complete comment form on page detail

--- vidshare/views.py
+++ vidshare/views.py
@@ -1,8 +1,11 @@
 
+
 from unicodedata import category
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Category, Video, Author
-from django.views.generic import ListView, DetailView, View
+from django.urls import reverse
+from .models import Category, Video, Author, Comment
+from django.views.generic import ListView, View
 from random import shuffle
 from .forms import CommentForm
 
@@ -25,19 +28,53 @@ class HomePage(ListView):
 
     
     
-class DetailView(DetailView):
-    template_name = "vidshare/detail-page.html"
-    model = Video
-    context_object_name = "main_video"
+class DetailView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        for category in self.object.category.all():
-            print(category)
-            context["recommended"] = Video.objects.all().filter(category__option=category)
-        context["comment_form"] = CommentForm()
+    def get(self, request, slug):
+        video = Video.objects.get(slug=slug)
+        context = {
+            "main_video": video,
+            "comment_form": CommentForm(),
+            "comments": Comment.objects.all().filter(video__slug=slug)
+        }
+        for category in video.category.all():
+            context["recommended"] = Video.objects.all().filter(category__option=category).exclude(slug=slug)
+
+        return render(request, 'vidshare/detail-page.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        video = Video.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.video = video
+            comment.save()
+            return HttpResponseRedirect(reverse('detail-page', args=[slug]))
+
+        context = {
+            "main_video": video,
+            "comment_form": comment_form,
+            "comments": Comment.objects.all().filter(video__slug=slug)
+        }
+
+        for category in video.category.all():
+            context["recommended"] = Video.objects.all().filter(category__option=category).exclude(slug=slug)
+
+        return render(request, 'vidshare/detail-page.html', context)
+
+    # template_name = "vidshare/detail-page.html"
+    # model = Video
+    # context_object_name = "main_video"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     for category in self.object.category.all():
+    #         print(category)
+    #         context["recommended"] = Video.objects.all().filter(category__option=category)
+    #     context["comment_form"] = CommentForm()
         
-        return context
+    #     return context
     
 
 class VideosView(ListView):
@@ -55,9 +92,17 @@ class VideosView(ListView):
         return context
     
     
-class AuthorView(DetailView):
-    template_name = "vidshare/author-page.html"
-    model = Author
-    context_object_name = 'author'
+class AuthorView(View):
+    def get(self, request, slug):
+        author = Author.objects.get(slug=slug)
+        return render(request, "vidshare/author-page.html", context = {
+            "author": author
+        })
+
+    #DetailView
+
+    # template_name = "vidshare/author-page.html"
+    # model = Author
+    # context_object_name = 'author'
 
   
